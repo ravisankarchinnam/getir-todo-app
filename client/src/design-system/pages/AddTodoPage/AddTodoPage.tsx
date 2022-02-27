bfbf81d2a0cc52f7ccfd28eb3456b45d053ca34a
ravisankarchinnam <22351907+ravisankarchinnam@users.noreply.github.com>
2022-02-27 21:46:54 +0100

    add todo pages

--- client/src/design-system/pages/AddTodoPage/AddTodoPage.tsx
+++ client/src/design-system/pages/AddTodoPage/AddTodoPage.tsx
@@ -1,10 +1,73 @@
-import PageLayout from "design-system/templates/PageLayout";
 import React from "react";
+import {Card, FormInstance, Typography} from "antd";
+import {addTodoRequested, selectTodosLoading} from "store/slices/todoSlice";
+import DynamicForm from "design-system/organisms/DynamicForm";
+import {IDynamicFormFieldProps} from "design-system/organisms/DynamicFormField";
+import {useAppDispatch, useAppSelector} from "store/hooks";
+import PageLayout from "design-system/templates/PageLayout";
+import {ITodo} from "store/models";
+
+const fields: IDynamicFormFieldProps[] = [
+  {
+    name: "name",
+    type: "input",
+    placeholder: "What's in your mind?",
+    rules: [{required: true, message: "This field is required"}],
+  },
+  {
+    name: "description",
+    type: "textarea",
+    placeholder: "Would you like a description?",
+    isTextArea: true,
+    rows: 4,
+  },
+  {
+    name: "deadline",
+    type: "date",
+    placeholder: "Add a dealine",
+  },
+];
+
+const styles = {
+  root: {
+    maxWidth: 600,
+    margin: "0 auto",
+  },
+};
 
 const AddTodoPage: React.FC = () => {
+  const loading = useAppSelector(selectTodosLoading);
+  const dispatch = useAppDispatch();
+
+  const onFinish = (form: FormInstance<any>) => {
+    const todo = {
+      name: form.getFieldValue("name"),
+      description: form.getFieldValue("description"),
+      deadline: form.getFieldValue("deadline")?.format(),
+    } as ITodo;
+    dispatch(addTodoRequested(todo));
+  };
+
+  const formData = {
+    name: "todo-add-form",
+    fields,
+    onFinish,
+    submitProps: {
+      label: "Add Todo",
+      loading,
+    },
+  };
+
   return (
     <PageLayout>
-      <h1>Add todo page</h1>
+      <div style={styles.root}>
+        <Typography.Title level={1}>Just Todo</Typography.Title>
+        <Typography.Text type="secondary">fill in the form and press Enter or press the button.</Typography.Text>
+
+        <Card title="Create a new todo">
+          <DynamicForm {...formData} />
+        </Card>
+      </div>
     </PageLayout>
   );
 };
--- client/src/design-system/pages/TodoListPage/TodoListPage.tsx
+++ client/src/design-system/pages/TodoListPage/TodoListPage.tsx
@@ -1,10 +1,47 @@
+import React, {useEffect} from "react";
+import {useHistory} from "react-router-dom";
+import {Typography, Card} from "antd";
+import {ArrowLeftOutlined} from "@ant-design/icons";
+
+import {useAppDispatch} from "store/hooks";
+import {fetchTodosRequested} from "store/slices/todoSlice";
+import TodoList from "design-system/organisms/TodoList";
 import PageLayout from "design-system/templates/PageLayout";
-import React from "react";
+import Button from "design-system/atoms/Button";
+
+const styles = {
+  backBtn: {
+    paddingLeft: 0,
+    marginBottom: 10,
+  },
+  list: {
+    maxWidth: 800,
+    margin: "0 auto",
+  },
+};
 
 const TodoListPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch(fetchTodosRequested());
+  }, [dispatch]);
+
   return (
     <PageLayout>
-      <h1>Todo List page</h1>
+      <Button
+        type="link"
+        onClick={() => history.push("/")}
+        icon={<ArrowLeftOutlined />}
+        size="large"
+        label="Back"
+        style={styles.backBtn}
+      />
+
+      <Card title={<Typography.Title level={1}>List of Todos</Typography.Title>} style={styles.list}>
+        <TodoList />
+      </Card>
     </PageLayout>
   );
 };
